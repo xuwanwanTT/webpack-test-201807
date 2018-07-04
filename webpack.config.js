60954ab8b45534c5a44aa8ea31a4df57eedf4291
肖丰 <xf@jusfoun.com>
2018-07-04 14:36:28 +0800

    feat:finish

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  module: 'development',
+  mode: 'development',
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './dist'
+  },
+  entry: {
+    js: './src/index.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(gif||svg||png||jpg)$/,
+        use: ['file-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html'
+    })
+  ]
+};
